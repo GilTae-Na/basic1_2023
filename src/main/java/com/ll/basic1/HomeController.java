615bc00a694d684d633cb37d9845134775dc2a65
nagt1997@gmail.com <nagt1997@gmail.com>
2023-03-14 11:46:13 +0900

    count 수정

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 개발자가 스프링부트에게 말한다.
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
+//점원이다 한명이 계속 일한다 = 객체가 하나만 생성되고 계속 일한다
 public class HomeController {
-    int count = 0;
+    private int count;
+
+    HomeController(){
+        this.count = -1;
+    }
     // @GetMapping("/home/main") 의 의미
     // 개발자가 스프링부트에게 말한다.
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
@@ -34,8 +39,10 @@ public class HomeController {
     }
 
     @GetMapping("/home/increase")
+    //클라이언트와 서버는 서로 String으로 통신한다. 다른것들 안댐
     @ResponseBody
-    public int showIncrease() {
-        return count++;
+    public int showIncrease() { //리턴되는 int는 String화 되어서 고객한테 돌아감
+        count++;
+        return count;
     }
 }
