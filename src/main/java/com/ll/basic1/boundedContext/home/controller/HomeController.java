0eaf6e06221dd6fa2ac6bf98474a325ac0795f73
nagt1997@gmail.com <nagt1997@gmail.com>
2023-03-15 11:12:49 +0900

    강사님 깃 참고 패키지 복구

--- src/main/java/com/ll/basic1/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/basic1/boundedContext/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ll.basic1;
+package com.ll.basic1.boundedContext.home.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,16 +19,15 @@ import java.util.*;
 // 개발자가 스프링부트에게 말한다.
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
-//점원이다 한명이 계속 일한다 = 객체가 하나만 생성되고 계속 일한다
 public class HomeController {
     private int count;
-    private int id;
-    List<Person> list = new ArrayList<>();
+    private List<Person> people;
 
-    HomeController(){
-        this.count = -1;
-        this.id = 1;
+    public HomeController() {
+        count = -1;
+        people = new ArrayList<>();
     }
+
     // @GetMapping("/home/main") 의 의미
     // 개발자가 스프링부트에게 말한다.
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
@@ -53,27 +52,21 @@ public class HomeController {
     }
 
     @GetMapping("/home/increase")
-    //클라이언트와 서버는 서로 String으로 통신한다. 다른것들 안댐
     @ResponseBody
-    public int showIncrease() { //리턴되는 int는 String화 되어서 고객한테 돌아감
+    public int showIncrease() { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
         count++;
         return count;
     }
 
+    @GetMapping("/home/plus")
+    @ResponseBody
     // @RequestParam 의 의미
     // 개발자가 스프링부트에게 말한다.
     // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
     // @RequestParam 는 생략가능
-    //(defaultValue = "0") 기본값설정, 값을 반드시 String으로 하자
-    //a는 URL에서 생략가능 => /home/plus?b=10 => 가능
-    //b는 URL에서 생략 불가능 => /home/plus?a=10 => 불가능(defaultValue 설정이 되어있지 않아서)
-    @GetMapping("/home/plus")
-    @ResponseBody
-    public int showPlus(@RequestParam(defaultValue = "0")int a, @RequestParam int b) {
-        return a+b;
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a + b;
     }
-
-
 
     @GetMapping("/home/returnBoolean")
     @ResponseBody
@@ -207,67 +200,39 @@ public class HomeController {
     }
 
     @GetMapping("/home/addPerson")
-    //사람등록
     @ResponseBody
-    public String addPerson(@RequestParam(defaultValue = "") String name, @RequestParam(defaultValue = "") int age) {
-        list.add(new Person(id, name, age));
-        return (id++) + "번 사람을 등록했습니다.";
-    }
+    public String addPerson(String name, int age) {
+        Person p = new Person(name, age);
 
-    @GetMapping("/home/removePerson")
-    //사람 보여주기
-    @ResponseBody
-    public List<Person> removePerson(@RequestParam(defaultValue = "0") int id) {
-        list.remove(id-1);
-        return list;
+        System.out.println(p);
+
+        people.add(p);
+
+        return "%d번 사람이 추가되었습니다.".formatted(p.getId());
     }
 
     @GetMapping("/home/people")
-    //사람 보여주기
     @ResponseBody
-    public List<Person> showPerson() {
-        return list;
+    public List<Person> showPeople() {
+        return people;
     }
 
-
-    @GetMapping("/home/removePerson2")
+    @GetMapping("/home/removePerson")
     @ResponseBody
-    //강사님 코드
-    public String removePerson2(int id) {
+    public String removePerson(int id) {
         // person -> person.getId() == id
         // 위 함수가 참인 엘리먼트(요소) 경우가 존재하면, 해당 요소를 삭제한다.
         // removed 에는 삭제수행여부가 저장된다.
         // 조건에 맞는걸 찾았고 삭제까지 되었다면 true, 아니면 false
-        boolean removed = list.removeIf(person -> person.getId() == id);
+        boolean removed = people.removeIf(person -> person.getId() == id);
 
         if (removed == false) {
             return "%d번 사람이 존재하지 않습니다.".formatted(id);
         }
 
-        /* 람다식을 안쓴 코드
-        for ( Person p : list ) {
-            if ( p.getId() == id ) list.remove(p);
-        }*/
-
         return "%d번 사람이 삭제되었습니다.".formatted(id);
     }
-    @GetMapping("home/modifyPerson")
-    @ResponseBody
-    public String modifyPerson(int id, String name, int age) {
-        //list.set(id-1, new Person(id, name, age));
 
-        for ( Person p : list ) {
-            if ( p.getId() == id ){
-                list.set(id-1, new Person(id, name, age));
-                return "%d번 사람이 수정되었습니다.".formatted(id);
-            }
-            else if( p.getId() != id) return "%d번 사람이 존재하지 않습니다.".formatted(id);
-        }
-        return "";
-    }
-
-    //강사님 코드 쓰기
-    /*
     @GetMapping("/home/modifyPerson")
     @ResponseBody
     public String modifyPerson(int id, String name, int age) {
@@ -285,10 +250,8 @@ public class HomeController {
         found.setAge(age);
 
         return "%d번 사람이 수정되었습니다.".formatted(id);
-    }*/
+    }
 
-    //쿠키
-    //스프링 부트는 브라우저(고객)를 구분하지 못한다.
     @GetMapping("/home/reqAndResp")
     @ResponseBody
     public void showReqAndResp(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -301,10 +264,11 @@ public class HomeController {
     public int showCookieIncrease(HttpServletRequest req, HttpServletResponse resp) throws IOException { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
         int countInCookie = 0;
 
+        // 고객이 가져온 쿠폰에서 count 쿠폰을 찾고 그 쿠폰의 값을 가져온다.
         if (req.getCookies() != null) {
             countInCookie = Arrays.stream(req.getCookies())
                     .filter(cookie -> cookie.getName().equals("count"))
-                    .map(cookie -> cookie.getValue())
+                    .map(Cookie::getValue)
                     .mapToInt(Integer::parseInt)
                     .findFirst()
                     .orElse(0);
@@ -312,16 +276,16 @@ public class HomeController {
 
         int newCountInCookie = countInCookie + 1;
 
+        // 고객이 가져온 count 쿠폰값에 1을 더한 쿠폰을 만들어서 고객에게 보낸다.
+        // 쉽게 말하면 브라우저(고객)에 저장되어 있는 count 쿠폰의 값을 1 증가시킨다.
+        // 이렇게 브라우저의 쿠키값을 변경하면 재방문시에 스프링부트가 다시 그 값을 받게 되어 있다.
         resp.addCookie(new Cookie("count", newCountInCookie + ""));
 
+        // 응답 본문
         return newCountInCookie;
     }
-
-
-
-
-
 }
+
 class Car {
     private final int id;
     private final int speed;
@@ -351,7 +315,7 @@ class Car {
         return relatedIds;
     }
 }
-//롬복 사용
+
 @AllArgsConstructor
 @Getter
 class CarV2 {
@@ -365,18 +329,6 @@ class CarV2 {
 @AllArgsConstructor
 @Getter
 @ToString
-//출력형식 String으로 만들어줌
-class Person{
-    private int id;
-    private String name;
-    private int age;
-
-    public int getId(int id) {
-        return this.id;
-    }
-}
-
-/* 강사님껏
 class Person {
     private static int lastId;
     private final int id;
@@ -393,4 +345,4 @@ class Person {
     Person(String name, int age) {
         this(++lastId, name, age);
     }
-}*/
+}
